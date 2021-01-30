af226fe0cba15c4d202566f0fbc8d80b5bfeb6f6
IvanAKim <Ivanalejandrokim@gmail.com>
2021-01-30 00:54:52 -0800

    Added Main Animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,10 @@ public class PlayerMovement : MonoBehaviour
     public Animator myAnimator;
     public SpriteRenderer myRenderer;
     private Rigidbody2D rb;
+    [SerializeField]
+    private bool crouch;
+    [SerializeField]
+    private bool jump;
     
 
     void Start() => rb = this.gameObject.GetComponent<Rigidbody2D>();
@@ -17,7 +21,12 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         OnMove();
-        OnJump(); 
+        CheckLanding();
+        OnJump();
+        OnCrouch();
+        CheckCrouching(crouch);
+       
+        
     }
 
     private void OnMove()
@@ -33,7 +42,8 @@ public class PlayerMovement : MonoBehaviour
         }
 
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
-        myAnimator.SetFloat("speed", Mathf.Abs(movement)); 
+        myAnimator.SetFloat("speed", Mathf.Abs(movement));
+        myAnimator.SetBool("isCrouching", crouch); 
 
     }
 
@@ -43,8 +53,39 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(new Vector3(0, jumpForce), ForceMode2D.Impulse);
+            jump = true;
+            myAnimator.SetBool("isJumping", true); 
         }
         
+    }
+
+    private void OnCrouch()
+    {
+        if(Input.GetButtonDown("Crouch") && !jump)
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false; 
+        }
+    }
+
+    
+    private void CheckCrouching(bool c)
+    {
+        myAnimator.SetBool("isCrouching", c);
+    }
+   
+
+    private void CheckLanding()
+    {
+         if (Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            jump = false;
+            myAnimator.SetBool("isJumping", false);
+        }
+
     }
 
 
