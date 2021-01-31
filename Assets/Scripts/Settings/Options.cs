a6b43a7ce35a4f888666d851667e8ce5349c48b9
IvanAKim <Ivanalejandrokim@gmail.com>
2021-01-31 14:50:09 -0800

    Main Menu Updated

--- Assets/Scripts/Settings/Options.cs
+++ Assets/Scripts/Settings/Options.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
-
+    public Inventory inventory;
     public void Resume()
     {
         
@@ -12,7 +13,14 @@ public class Options : MonoBehaviour
 
     public void MainMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
+    }
+
+
+    public void StartGame()
+    {
+        inventory.ClearInventory(); 
+        SceneManager.LoadScene("_mainHub"); 
     }
 
     public void QuitGame()
