ccff6a8d1e82d1247a2b52daf8a6cad32de7269a
JGerdes <gerdesjaren@gmail.com>
2021-01-31 15:29:29 -0800

    Blue water respawn points

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public GameObject player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.transform.position = respawnPoint.position;
+    }
+}
