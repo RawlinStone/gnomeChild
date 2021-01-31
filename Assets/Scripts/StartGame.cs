a70b77da6184b061be414bc2d87ba8f0a0c98512
JGerdes <gerdesjaren@gmail.com>
2021-01-31 14:38:29 -0800

    Main Menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    private void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(ChangeScene);
+    }
+
+    // Update is called once per frame
+    void ChangeScene()
+    {
+        Application.LoadLevel("_mainHub");
+    }
+}
