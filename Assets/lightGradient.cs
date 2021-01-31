8f044cf1379bae790a18c5123951f420b90527d4
Hollowfires <hollowfires@hotmail.com>
2021-01-30 22:41:11 -0800

    fairy light and trying to setup pulsing light

---
+++ Assets/lightGradient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightGradient : MonoBehaviour
+{
+    float duration = 1.0f;
+    Color color0 = Color.yellow;
+    Color color1 = Color.cyan;
+
+    Light lt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+}
