00f4822a874fd2985a36bda0e71650198a936760
IvanAKim <Ivanalejandrokim@gmail.com>
2021-01-30 23:09:36 -0800

    Updated Player Prefab and option menu

---
+++ Assets/Scripts/Settings/Escape.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    public Options myOptions;
+    public PlayerMovement playerMovement;
+    public RopeSystem ropeSystem;
+    public PlayerInteracting playerInteracting;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            if (!myOptions.gameObject.activeSelf)
+            {
+                OpenOptions(); 
+            }
+            else
+            {
+                CloseOptions();
+            }
+        }
+    }
+
+    public void OpenOptions()
+    {
+        myOptions.gameObject.SetActive(true);
+        playerMovement.enabled = false;
+        ropeSystem.enabled = false;
+        playerInteracting.enabled = false;
+        Cursor.visible = true;
+    }
+
+    public void CloseOptions()
+    {
+        myOptions.gameObject.SetActive(false);
+        playerMovement.enabled = true;
+        ropeSystem.enabled = true;
+        playerInteracting.enabled = true;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/Settings/Options.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+
+    public void Resume()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
